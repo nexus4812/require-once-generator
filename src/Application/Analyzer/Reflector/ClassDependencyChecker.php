9a69a96696a328b9a7a19a65c7ee8443be03f76c
nexus4812 <powerbatteryboy@gmail.com>
2023-05-01 13:43:03 +0900

    wip

---
+++ src/Application/Analyzer/Reflector/ClassDependencyChecker.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace RequireOnceGenerator\Application\Analyzer\Reflector;
+
+use ReflectionClass;
+
+class ClassDependencyChecker
+{
+    private array $dependencies = [];
+
+    /**
+     * 指定されたクラスの依存するクラスを再帰的に取得する
+     *
+     * @param ReflectionClass $class 取得するクラス
+     * @return array 依存するクラスの配列
+     */
+    public function getClassDependencies(ReflectionClass $class): array
+    {
+        $this->getConstructorDependencies($class);
+        $this->getPropertyDependencies($class);
+        $this->getMethodDependencies($class);
+        $this->resolveDependenciesRecursively();
+        return array_unique($this->getDependencies());
+    }
+
+    /**
+     * コンストラクタの依存するクラスを取得する
+     *
+     * @param ReflectionClass $class 取得するクラス
+     */
+    private function getConstructorDependencies(ReflectionClass $class): void
+    {
+        $constructor = $class->getConstructor();
+        if ($constructor === null) {
+            return;
+        }
+
+        $params = $constructor->getParameters();
+        foreach ($params as $param) {
+            $type = $param->getType();
+            if ($type !== null) {
+                $this->dependencies[] = $type->getName();
+            }
+        }
+    }
+
+    /**
+     * プロパティの依存するクラスを取得する
+     *
+     * @param ReflectionClass $class 取得するクラス
+     */
+    private function getPropertyDependencies(ReflectionClass $class): void
+    {
+        $properties = $class->getProperties();
+        foreach ($properties as $property) {
+            $type = $property->getType();
+            if ($type !== null) {
+                $this->dependencies[] = $type->getName();
+            }
+        }
+    }
+
+    /**
+     * メソッドの依存するクラスを取得する
+     *
+     * @param ReflectionClass $class 取得するクラス
+     */
+    private function getMethodDependencies(ReflectionClass $class): void
+    {
+        $methods = $class->getMethods();
+        foreach ($methods as $method) {
+            $params = $method->getParameters();
+            foreach ($params as $param) {
+                $type = $param->getType();
+                if ($type !== null) {
+                    $this->dependencies[] = $type->getName();
+                }
+            }
+        }
+    }
+
+    /**
+     * 依存するクラスを再帰的に解決する
+     */
+    private function resolveDependenciesRecursively(): void
+    {
+        $dependencies = $this->getDependencies();
+        foreach ($dependencies as $dependency) {
+            $class = new ReflectionClass($dependency);
+            $this->getConstructorDependencies($class);
+            $this->getPropertyDependencies($class);
+            $this->getMethodDependencies($class);
+        }
+    }
+
+    /**
+     * 依存するクラスの配列を取得する
+     *
+     * @return array 依存するクラスの配列
+     */
+    private function getDependencies(): array
+    {
+        return $this->dependencies;
+    }
+}
