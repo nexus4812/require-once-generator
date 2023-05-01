1bd3aef7ea41be976baf0459ce102107a3f37039
nexus4812 <powerbatteryboy@gmail.com>
2023-05-01 12:29:59 +0900

    wip

---
+++ src/Application/Analyzer/Visitor/ClassDependencyVisitor.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace RequireOnceGenerator\Application\Analyzer\Visitor;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\New_;
+use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Namespace_;
+use PhpParser\Node\Stmt\Use_;
+use PhpParser\NodeVisitorAbstract;
+
+class ClassDependencyVisitor extends NodeVisitorAbstract
+{
+    /**
+     * @var array<string, string>
+     */
+    private array $classes = [];
+    private string $currentNamespace = '';
+    private array $uses;
+
+    public function enterNode(Node $node)
+    {
+        if ($node instanceof Namespace_) {
+            $this->currentNamespace = $node->name->toString() . '\\';
+            return;
+        }
+
+        if ($node instanceof Use_) {
+            foreach ($node->uses as $use) {
+                $this->uses[$use->getAlias()->toString()] = $use->name->toString();
+            }
+            return;
+        }
+
+        if (!$node instanceof Class_ && !$node instanceof New_ && !$node instanceof StaticCall) {
+            return;
+        }
+
+        $className = $node->class->toString();
+        if (!$this->isFullyQualified($className) && $this->currentNamespace !== '') {
+            $className = $this->currentNamespace . $className;
+        }
+
+        if (!in_array($className, $this->classes)) {
+            $this->classes[] = $className;
+        }
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function getClasses(): array
+    {
+        return $this->classes;
+    }
+
+    private function isFullyQualified(string $className): bool
+    {
+        return str_starts_with($className, '\\');
+    }
+
+    public function getFullUseClass()
+    {
+        $result = [];
+        foreach ($this->getClasses() as $class) {
+            $result[] = array_key_exists($class, $this->uses) ?
+                 $this->uses[$class] :
+                 $class;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    public function getUses(): array
+    {
+        return $this->uses;
+    }
+}
